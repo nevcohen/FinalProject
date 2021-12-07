0dbb7ede68543b962a94937e4aab13d34f5d2a38
yosef <yamrusi@g.jct.ac.il>
2021-12-07 14:47:58 +0200

    3132

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.finalproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, open_window)
+        startActivity(intent)
     }
 
 }
